37b1df7283e91c42488e6c03af22ef21475fdd6b
p.p <38204935+p0mp4n@users.noreply.github.com>
2023-04-07 22:23:19 +0900

    [계산기 웹 프로그램]HTTP protocol의 구조 파악

--- src/main/java/org/example/CustomWebApplicationServer.java
+++ src/main/java/org/example/CustomWebApplicationServer.java
@@ -3,9 +3,10 @@ package org.example;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class CustomWebApplicationServer {
     private final int port;
@@ -29,6 +30,22 @@ public class CustomWebApplicationServer {
                 /**
                  * Step1. 사용자 요청을 메인 Thread가 처리하도록 한다.
                  */
+
+                try(InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()) {
+                    BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+                    DataOutputStream dos = new DataOutputStream(out);
+
+                    // HTTP프로토콜의 생김새 파악
+                    String line;
+                    while((line = br.readLine()) != "") {
+                        System.out.println(line);
+                        // RequestLine -> TODO:스플릿을 통해 구현
+                        // Header
+                        // BlankLine
+                        // Body
+                    }
+
+                }
             }
         }
     }
