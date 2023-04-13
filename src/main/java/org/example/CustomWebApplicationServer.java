333fcbba5b95c5495a39ece7f1d0fe62494700ef
p.p <38204935+p0mp4n@users.noreply.github.com>
2023-04-13 20:53:31 +0900

    [계산기 웹 프로그램]Thread pool 적용

--- src/main/java/org/example/CustomWebApplicationServer.java
+++ src/main/java/org/example/CustomWebApplicationServer.java
@@ -9,10 +9,13 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CustomWebApplicationServer {
     private final int port;
 
+    private final ExecutorService excutorService = Executors.newFixedThreadPool(10);
     private static final Logger logger = LoggerFactory.getLogger(CustomWebApplicationServer.class);
 
     public CustomWebApplicationServer(int port) {
@@ -27,13 +30,11 @@ public class CustomWebApplicationServer {
             logger.info("[CustomWebApplicationServer] waiting for client.");
 
             /**
-             * Step2 - 사용자 요청이 들어올 때마다 Thread를 새로 생성해서 사용자 요청을 처리하도록 한다.
-             * 문제점: Thread는 각 thread가 생성될때마다 스택메모리를 할당받는데, 이 메모리를 할당받는 작업은 큰 비용을 필요로 한다
-             * 그 결과, 최악의 경우 서버의 리소스가 감당하지 못하고 다운되어 버릴 수도 있다.
+             * Step3 - Thread pool을 적용해 안정적인 서비스가 가능하도록 한다
              */
             while ((clientSocket = serverSocket.accept()) != null) {
                 logger.info("[CustomWebApplicationServer] client connected.");
-                new Thread(new ClientRequestHandler(clientSocket)).start();
+                excutorService.execute(new ClientRequestHandler(clientSocket));
             }
         }
     }
